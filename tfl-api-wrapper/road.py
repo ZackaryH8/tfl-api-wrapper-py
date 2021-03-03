b292744f6a1cca3053d5fe82d6652b0a355a21f9
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-03-03 17:47:53 +0000

    Add Road API

---
+++ tfl-api-wrapper/road.py
@@ -0,0 +1,76 @@
+from tfl import tflAPI
+
+
+class Road(tflAPI):
+    """Road from Unified API"""
+
+    def getAll(self):
+        """Get all roads managed by TfL"""
+
+        return super(Road, self).sendRequestUnified(
+            '/Road', {}
+        )
+
+    def getByID(self, ids):
+        """
+        Get the road with the specified ID (Eg. A1)
+
+        :param ids: ID(s) of the road(s). Eg. ['A1']
+        """
+
+        return super(Road, self).sendRequestUnified(
+            f'/Road/{self.arrayToCSV(ids)}', {}
+        )
+
+    def getStatusByID(self, ids, startDate, endDate):
+        """
+        Gets the specified roads with the status aggregated over the date range specified, or now until the end of today if no dates are passed.
+
+        :param ids: ID(s) of the road(s). Eg. ['A1']
+        :param startDate:
+        :param endDate:
+        """
+
+        return super(Road, self).sendRequestUnified(
+            f'/Road/{self.arrayToCSV(ids)}', {'startDate': self.getRFC3339(startDate), 'endDate': self.getRFC3339(endDate)}
+        )
+
+    def getAllStreetDisruption(self, startDate, endDate):
+        """
+        Gets a list of disrupted streets
+
+        @param startDate:
+        @param endDate:
+        """
+        return super(Road, self).sendRequestUnified(
+            '/Road/all/Street/Disruption',
+            {'startDate': self.getRFC3339(startDate), 'endDate': self.getRFC3339(endDate)}
+        )
+
+    def getAllDisruptionsByID(self, ids, stripContent: bool):
+        """
+        Gets a list of disrupted streets
+
+        @param ids:
+        @param stripContent: When true, removes every property/node except for id, point, severity, severityDescription,  startDate, endDate, corridor details, location and comments.
+        """
+        return super(Road, self).sendRequestUnified(
+            f'/Road/all/Disruption/{self.arrayToCSV(ids)}',
+            {'stripContent': stripContent}
+        )
+
+    def getCategories(self):
+        """Gets a list of valid RoadDisruption categories"""
+
+        return super(Road, self).sendRequestUnified(
+            '/Road/Meta/Categories',
+            {}
+        )
+
+    def getSeverities(self):
+        """Gets a list of valid RoadDisruption severity codes"""
+
+        return super(Road, self).sendRequestUnified(
+            '/Road/Meta/Severities',
+            {}
+        )
