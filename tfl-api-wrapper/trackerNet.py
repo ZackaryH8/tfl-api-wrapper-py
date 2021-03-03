bcce3e2d676a09b9510bbd34cb4bcaf4e2009795
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-03-03 03:30:13 +0000

    Add basic TrackerNet API

---
+++ tfl-api-wrapper/trackerNet.py
@@ -0,0 +1,43 @@
+from tfl import tflAPI
+
+
+class TrackerNet(tflAPI):
+    """TrackerNet API"""
+
+    def getPredictionSummary(self, line: str):
+        """Get detailed train prediction information for a nominated station on a nominated line within 100 minute range"""
+
+        return super(TrackerNet, self).sendRequestTrackerNet(
+            f"/PredictionSummary/${line}"
+        )
+
+    def getPredictionDetailed(self, line: str, stationCode: str):
+        """Get detailed train prediction information for a nominated station on a nominated line within 100 minute range"""
+
+        return super(TrackerNet, self).sendRequestTrackerNet(
+            f"/PredictionDetailed/{line}/{stationCode}"
+        )
+
+    def getAllLinesStatus(self, incidentsOnly: bool):
+        """
+        Get the status of all lines on the network indicating any delays, disruptions or suspensions on the lines.
+
+        :param incidentsOnly: Get station status information for stations with incidents only
+        """
+
+        incidentsOnlyCheck = '/IncidentsOnly' if incidentsOnly else ''
+        return super(TrackerNet, self).sendRequestTrackerNet(
+            f"/LineStatus{incidentsOnlyCheck}"
+        )
+
+    def getAllStationStatus(self, incidentsOnly: bool):
+        """
+        Get station status information for all stations
+
+        :param incidentsOnly: Get station status information for stations with incidents only
+        """
+
+        incidentsOnlyCheck = '/IncidentsOnly' if incidentsOnly else ''
+        return super(TrackerNet, self).sendRequestTrackerNet(
+            f"/StationStatus{incidentsOnlyCheck}"
+        )
