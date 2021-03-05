5bff556e5cd917aa1fd20bbe1a7967fac02a426d
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-03-05 02:19:19 +0000

    Bump Version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = pathlib.Path(__file__).parent.parent
 
 setup(
     name="tflwrapper",
-    version="1.0.0",
+    version="1.0.1",
     description="A wrapper for the Transport for London Unified API and TrackerNet",
     long_description=(HERE / "README.md").read_text(),
     long_description_content_type="text/markdown",
