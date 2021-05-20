670f6c7573d7ff71721cdec1effde1e78bf7053b
ZackaryH8 <zackary12389@yahoo.co.uk>
2021-05-20 08:45:10 +0000

    Added accidentStats class

---
+++ tflwrapper/accidentStats.py
@@ -0,0 +1,15 @@
+from .tfl import tflAPI
+
+
+class accidentStats(tflAPI):
+    """Accident from Unified API"""
+
+    def getAllByYear(self, year: int):
+        """
+        Gets all accident details for accidents occuring in the specified year
+
+        :param year: The year for which to filter the accidents on.
+        """
+        return super(accidentStats, self).sendRequestUnified(
+            f"/AccidentStats/{year}", {}
+        )
