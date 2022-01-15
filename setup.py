d00bd4aa1909eb417938c64c8b38a8c7bab39412
Zack <zackary12389@yahoo.co.uk>
2022-01-15 20:07:15 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import pathlib
 from setuptools import setup, find_packages
 
-HERE = pathlib.Path(__file__).parent.parent
+HERE = pathlib.Path(__file__).parent
 
 setup(
     name="tflwrapper",
-    version="1.0.2",
+    version="1.1.2",
     description="A wrapper for the Transport for London Unified API and TrackerNet",
     long_description=(HERE / "README.md").read_text(),
     long_description_content_type="text/markdown",
